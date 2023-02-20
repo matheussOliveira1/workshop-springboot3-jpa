94f3305ce500b4be3ccf3b56720da93a374ff9f3
matheussOliveira1 <matheus.aoliveira@sptech.school>
2023-02-20 10:23:01 -0300

    Camada de serviço, registro de componentes

---
+++ springproject/src/main/java/com/springstudy/springproject/services/UserService.java
@@ -0,0 +1,27 @@
+package com.springstudy.springproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springstudy.springproject.entities.User;
+import com.springstudy.springproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
