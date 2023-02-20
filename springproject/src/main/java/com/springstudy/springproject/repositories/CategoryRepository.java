8deb780060ea7fa505904ed86884560ecd86c97b
matheussOliveira1 <matheus.aoliveira@sptech.school>
2023-02-20 11:50:47 -0300

    Entidade Category

---
+++ springproject/src/main/java/com/springstudy/springproject/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.springstudy.springproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springstudy.springproject.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{}
